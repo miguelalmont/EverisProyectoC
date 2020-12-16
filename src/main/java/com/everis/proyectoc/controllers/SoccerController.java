b3c040de0b0e97887515ecb5d8b74eada0df1d93
[LuRoGlez] <[rosell.golui20@cadiz.salesianos.edu]>
2020-12-16 11:36:12 +0100

    haciendo controller

--- src/main/java/com/everis/proyectoc/controllers/SoccerController.java
+++ src/main/java/com/everis/proyectoc/controllers/SoccerController.java
@@ -1,5 +1,38 @@
 package com.everis.proyectoc.controllers;
 
-public class SoccerController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("*")
+public class SoccerController {
+	
+	@Autowired
+	SoccerServiceI soccerService;
+	
+	/**
+	 * Escucha cualquier ruta y muestra la vista del índice.
+	 * 
+	 * @param model
+	 * @return String
+	 */
+	@GetMapping
+	public String showIndex(Model model) {
+		return "index";
+	}
+	/**
+	 * Escucha la ruta "ranking" y muestra la clasificación de los equipos
+	 * @param model
+	 * @return String
+	 */
+	@GetMapping("/ranking")
+	public String showRanking(Model model) {
+		
+		model.addAllAttributes("rankingTeams", soccerService.getRanking)
+	}
+	
+	
 }
