b7a077c08da2c720fa443966edc8c664aed8e4c6
[LuRoGlez] <[rosell.golui20@cadiz.salesianos.edu]>
2020-12-17 09:36:17 +0100

    comentarios terminados

--- src/main/java/com/everis/proyectoc/services/SoccerGamesServiceI.java
+++ src/main/java/com/everis/proyectoc/services/SoccerGamesServiceI.java
@@ -8,15 +8,33 @@ import com.everis.proyectoc.repositories.SoccerGames;
 
 @Service
 public interface SoccerGamesServiceI {
-
+	/**
+	 * Metodo para insertar nuevos partidos
+	 * @param game
+	 */
 	public void addGame(final SoccerGames game);
-
+	/**
+	 * Metodo que devuelve todos los partidos
+	 * @return List<SoccerGames>
+	 */
 	public List<SoccerGames> getAllGames();
-
+	
+	/**
+	 * Método que busca partidos por su ID
+	 * @param gameId
+	 * @return SoccerGame
+	 */
 	public SoccerGames getGameByID(final int gameId);
-
+	
+	/**
+	 * Metodo para eliminar partidos
+	 * @param gameId
+	 */
 	public void removeGameByID(final int gameId);
-
+	/**
+	 * Metodo para modificar partidos
+	 * @param game
+	 */
 	public void modifyGame(final SoccerGames game);
 
 }
